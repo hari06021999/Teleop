e09804cb95a6bd51dba6c9966558718f468102cc
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-07-21 11:42:03 +0530

    Update arduino_motor.ino

--- arduino_motor.ino
+++ arduino_motor.ino
@@ -3,12 +3,17 @@
 #include <math.h>
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
-#include <SoftwareSerial.h>
-SoftwareSerial portOne(11, 12);
+#include <std_msgs/Bool.h>
+#include <std_msgs/Int16.h>
+#include <PWM.h>
+//#include <SoftwareSerial.h>
+//SoftwareSerial portOne(11, 12);
 // The min amount of PWM the motors need to move. Depends on the battery, motors and controller.
 // The max amount is defined by PWMRANGE in Arduino.h
-#define PWM_MIN -255
-#define PWMRANGE 255
+#define PWM_MIN 80
+#define PWMRANGE 120
+int frequency_right = 350; //frequency (in Hz)
+int frequency_left = 400; //frequency (in Hz)
 
 // Declare functions
 void setupPins();
@@ -16,46 +21,60 @@ void setupSerial();
 void setupWiFi();
 bool rosConnected();
 void onTwist(const geometry_msgs::Twist &msg);
+void onfront(const std_msgs::Bool &msg);
+void onfont_multi(const std_msgs::Int16 &msg);
 float mapPwm(float x, float out_min, float out_max);
+void brake(void);
 
 // Pins
-const uint8_t R_PWM = 5;
-const uint8_t R_BACK = 6;
-const uint8_t R_FORW = 7;
-const uint8_t L_BACK = 8;
-const uint8_t L_FORW = 9;
-const uint8_t L_PWM = 10;
+const uint8_t R_PWM = 9;//D3
+const uint8_t right_relay = 2;// D2 RIGHT 1ST MOTOR
+const uint8_t left_relay = 4;// D4 RIGHT 2ND MOTOR
+const uint8_t L_PWM = 3;//D9
+const uint8_t front_light = 5;
+const uint8_t front_light_white = 6;
+const uint8_t front_light_yellow = 7;
+const uint8_t brake_light = 1;
 
 
 // ROS serial server
 
 ros::NodeHandle node;
-ros::Subscriber<geometry_msgs::Twist> sub("/cmd_vel", &onTwist);
+ros::Subscriber<std_msgs::Bool> sub_light1("front/light",&onfront);
+ros::Subscriber<std_msgs::Int16> sub_light2("front/light/multi",&onfont_multi);
+ros::Subscriber<geometry_msgs::Twist> sub("/linear/angular", &onTwist);
 
 bool _connected = false;
 
 void setup()
 {
+  InitTimersSafe(); 
+  pinMode(L_PWM, OUTPUT);
+  pinMode(R_PWM, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+  InitTimersSafe();
+  bool success = SetPinFrequencySafe(L_PWM, frequency_left);
+  SetPinFrequencySafe(R_PWM, frequency_right);
+  
   setupPins();
-  portOne.begin(9600);
-  portOne.println("hello Wlcome");
-  pinMode(13, OUTPUT);
-  digitalWrite(13,LOW);
   node.initNode();
   node.subscribe(sub);
+  node.subscribe(sub_light1);
+  node.subscribe(sub_light2);
   
 }
 
 void setupPins()
 {
-  
-
-  pinMode(L_PWM, OUTPUT);
-  pinMode(L_FORW, OUTPUT);
-  pinMode(L_BACK, OUTPUT);
-  pinMode(R_PWM, OUTPUT);
-  pinMode(R_FORW, OUTPUT);
-  pinMode(R_BACK, OUTPUT);
+   
+  pinMode(brake_light,OUTPUT);
+  pinMode(right_relay, OUTPUT);
+  pinMode(left_relay, OUTPUT);
+   pinMode(front_light, OUTPUT);
+  pinMode(front_light_white, OUTPUT);
+  pinMode(front_light_yellow, OUTPUT);
   stop();
   
 }
@@ -64,31 +83,34 @@ void setupPins()
 
 void stop()
 {
-  digitalWrite(L_FORW, 0);
-  digitalWrite(L_BACK, 0);
-  digitalWrite(R_FORW, 0);
-  digitalWrite(R_BACK, 0);
-  analogWrite(L_PWM, 0);
-  analogWrite(R_PWM, 0);
+    digitalWrite(brake_light, HIGH);
+  digitalWrite(right_relay, HIGH);
+  digitalWrite(left_relay, HIGH);
+  digitalWrite(front_light, HIGH);
+  digitalWrite(front_light_white, HIGH); 
+  digitalWrite(front_light_yellow, HIGH);
+    pwmWrite(L_PWM, 0);
+  pwmWrite(R_PWM, 0);
+  
 }
 
 void onTwist(const geometry_msgs::Twist &msg)
 {
+
   if (!_connected)
   {
     stop();
     return;
   }
-   digitalWrite(13,HIGH);
-   digitalWrite(13,LOW);
-  portOne.print("Linear:");
-  portOne.println(msg.linear.x);
-  portOne.print("Angular:");
-  portOne.println(msg.angular.z);
+if(msg.linear.x>0||msg.linear.x<0)
+{
   // Cap values at [-1 .. 1]
   float x = max(min(msg.linear.x, 1.0f), -1.0f);
   float z = max(min(msg.angular.z, 1.0f), -1.0f);
-
+  if(x==0 && z==0)
+  {
+    brake();
+  }
   // Calculate the intensity of left and right wheels. Simple version.
   // Taken from https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
   float l = (msg.linear.x - msg.angular.z) / 2;
@@ -97,26 +119,86 @@ void onTwist(const geometry_msgs::Twist &msg)
   // Then map those values to PWM intensities. PWMRANGE = full speed, while PWM_MIN = the minimal amount of power at which the motors begin moving.
   uint16_t lPwm = mapPwm(fabs(l), PWM_MIN, PWMRANGE);
   uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
-  digitalWrite(13,HIGH);
-   digitalWrite(13,LOW);
-  // Set direction pins and PWM
-  digitalWrite(L_FORW, l > 0);
-  digitalWrite(L_BACK, l < 0);
-  digitalWrite(R_FORW, r > 0);
-  digitalWrite(R_BACK, r < 0);
-  analogWrite(L_PWM, lPwm);
-  analogWrite(R_PWM, rPwm);
-  portOne.print("lpwm:");
-  portOne.println(lPwm);
-  portOne.print("rpwm:");
-  portOne.println(rPwm);
-}
 
+  // Set direction pins and PWM
+       digitalWrite(left_relay, l>0);
+       digitalWrite(right_relay, r>0);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  for(int i=25,j=25;i<lPwm,j<rPwm;i++,j++)
+  {
+   
+   pwmWrite(L_PWM, i);
+   pwmWrite(R_PWM, j);
+   delay(100);
+  }
+}
+else
+{
+  digitalWrite(LED_BUILTIN, LOW);
+  pwmWrite(L_PWM, 0);
+  pwmWrite(R_PWM, 0);
+}
+}
+void onfront(const std_msgs::Bool &msg)
+{
+  int check = msg.data;
+  if(check == 1)
+  {
+   
+
+    digitalWrite(front_light, LOW);
+   
+  }
+   if(check == 0)
+  {
+    digitalWrite(front_light, HIGH);
+  
+  }
+}
+void onfont_multi(const std_msgs::Int16 &msg)
+{
+  int check = msg.data;
+  if(check == 1)
+  {
+   
+    digitalWrite(front_light_white, LOW);
+    digitalWrite(front_light_yellow, HIGH);
+
+  }
+   if(check == 2)
+  {
+    digitalWrite(front_light_yellow, LOW);
+    digitalWrite(front_light_white, HIGH);
+  }
+  if(check == 0)
+  {
+    digitalWrite(front_light_white, HIGH);
+    digitalWrite(front_light_yellow, HIGH);
+  }
+}
+void brake(void)
+{
+ 
+        digitalWrite(brake_light, LOW);
+//        delay(1000);
+        digitalWrite(brake_light, HIGH);
+  
+}
 void loop()
 {
   if (!rosConnected())
     stop();
   node.spinOnce();
+//  while(1)
+//  {
+//    digitalWrite(LED_BUILTIN, HIGH);
+//    delay(500);
+//    digitalWrite(LED_BUILTIN, LOW);
+//    delay(500);
+//     if (!rosConnected())
+//        node.spinOnce();
+//  }
 }
 
 bool rosConnected()
@@ -127,7 +209,7 @@ bool rosConnected()
   {
     _connected = connected;
   
-    portOne.println(connected ? "ROS connected" : "ROS disconnected");
+   // portOne.println(connected ? "ROS connected" : "ROS disconnected");
   }
   return connected;
 }
