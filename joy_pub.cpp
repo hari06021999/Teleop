710f94adf726552e4c11dcb64d83fea260305199
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-07-21 10:06:34 +0530

    Update joy_pub.cpp

--- joy_pub.cpp
+++ joy_pub.cpp
@@ -7,71 +7,133 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 #include <stdio.h>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+#include <termios.h>
+#include <ctime>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Int16.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 
-class TeleopTurtle
-{
-public:
-  TeleopTurtle();
 
-private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+ 
+	
+class RemoteTeleop {
 
-  ros::NodeHandle nh_;
-
-  int linear_, angular_;
-  double l_scale_, a_scale_;
-  ros::Publisher vel_pub_;
-  ros::Subscriber joy_sub_;
-
+    private:
+   
+    ros::Publisher pub_light;
+    ros::Publisher pub_multicolor;
+    ros::Publisher pub;
+    ros::Subscriber joystick_subscriber;
+    ros::Subscriber button1_subscriber;
+    ros::Subscriber button2_subscriber;
+   
+      int count_1=0;
+      int count_2=0;
+      int count_3=0;
+    public:
+  
+        
+     
+      RemoteTeleop(ros::NodeHandle *nh) {
+        
+	pub_light = nh->advertise<std_msgs::Int16>("front/light",10);
+        pub_multicolor = nh->advertise<std_msgs::Int16>("front/light/multi",10);
+        pub = nh->advertise<geometry_msgs::Twist>("/linear/angular", 10);    
+        joystick_subscriber = nh->subscribe("/cmd_vel", 2, &RemoteTeleop::callback_joy, this);
+        button1_subscriber = nh->subscribe("btn1topic", 2, &RemoteTeleop::callback_button1, this);
+	button2_subscriber = nh->subscribe("btn2topic", 2, &RemoteTeleop::callback_button2, this);
+       
+           
+    }
+ 
+    void callback_joy(const geometry_msgs::Twist& msg) {
+       
+        printf("Linear:%lf , Angular:%lf\n",msg.linear.x,msg.angular.z);
+        pub.publish(msg);
+    }
+    
+    
+   void callback_button1(const std_msgs::Bool& msg) 
+   {
+        	
+     bool light_1=false;
+     std_msgs::Bool value;
+      light_1= msg.data;
+     if(light_1==true)
+     {
+       count_1++;
+     } 
+     if(count_1==1 && light_1==true)
+     {
+      light_1=true;
+      value.data=true;
+      pub_light.publish(value);
+      ROS_INFO("Front top Light :%d",light_1);
+     }
+     if(count_1==2 && light_1==true)
+     {
+       
+       light_1=false;
+        value.data=false;
+       pub_light.publish(value);
+      ROS_INFO("Front top Light:%d",light_1);
+      count_1=0;
+     }
+    
+ 
+   }
+   void callback_button2(const std_msgs::Bool& msg) 
+   {
+        	
+     bool light_2=false;
+     std_msgs::Int16 value;
+      light_2= msg.data;
+     if(light_2==true)
+     {
+       count_2++;
+     } 
+     if(count_2==1 && light_2==true)
+     {
+      light_2=true;
+      value.data=1;
+      pub_multicolor.publish(value);
+      ROS_INFO("White color :%d",light_2);
+     }
+     if(count_2==2 && light_2==true)
+     {
+       
+       light_2=true;
+        value.data=2;
+       pub_multicolor.publish(value);
+      ROS_INFO("Yellow color :%d",light_2);
+     
+     }
+    if(count_2==3 && light_2==true)
+     {
+       
+       light_2=false;
+        value.data=0;
+       pub_multicolor.publish(value);
+      ROS_INFO("Lights off:%d",light_2);
+      count_2=0;
+     }
+ 
+   }
+   
+    
 };
 
-
-TeleopTurtle::TeleopTurtle():
-  linear_(1),
-  angular_(2)
-{
-
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
-
-
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
-ROS_WARN("inside published");
-
-  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
-  
- // while(ros::ok())
- // {
- // geometry_msgs::Twist twist;
- // twist.angular.z = 0;//a_scale_*joy->axes[angular_]
- // twist.linear.x = 0;//l_scale_*joy->axes[linear_]
- // ROS_INFO("published");
- // vel_pub_.publish(twist);
-  //sleep(1);
- // }
-
+int main (int argc, char **argv)
+{ 
+    ros::init(argc, argv, "teleop");
+    ros::NodeHandle nh;
+    RemoteTeleop nc = RemoteTeleop(&nh);
+    ros::spin();
 }
 
-void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
-{
-  
-  geometry_msgs::Twist twist;
-  twist.angular.z = a_scale_*joy->axes[angular_];
-  twist.linear.x = l_scale_*joy->axes[linear_];
-  printf("Anglular:%lf ,Linear:%lf \n",twist.angular.z,twist.linear.x);
-  vel_pub_.publish(twist);
- 
-  
- 
-}
-
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "teleop_turtle");
-  TeleopTurtle teleop_turtle;
-
-  ros::spin();
-}
