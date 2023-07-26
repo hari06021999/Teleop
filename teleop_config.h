a7adfa4ad59b51891d0f9e91557959d23d5dcf81
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-07-26 15:18:29 +0530

    Update teleop_config.h

--- teleop_config.h
+++ teleop_config.h
@@ -14,21 +14,21 @@ void onTwist(const geometry_msgs::Twist &msg);
 void onfront(const std_msgs::Bool &msg);
 void onfont_multi(const std_msgs::Int16 &msg);
 float mapPwm(float x, float out_min, float out_max);
-void brake(void);
 
 
 // Pins
-const uint8_t R_PWM = 9;//D3
-const uint8_t right_relay = 2;// D2 RIGHT 1ST MOTOR
-const uint8_t left_relay = 4;// D4 RIGHT 2ND MOTOR
-const uint8_t L_PWM = 8;//D8
-const uint8_t front_light = 5;
-const uint8_t front_light_white = 6;
-const uint8_t front_light_yellow = 7;
-const uint8_t brake_light = 1;
-const uint8_t forward_brake = 10;
-const uint8_t reverse_brake = 11;
-const uint8_t reverse_light = 12;
+const uint8_t R_PWM = 6;
+const uint8_t right_relay = 3;
+const uint8_t left_relay = 4;
+const uint8_t L_PWM = 7;
+const uint8_t front_light = 14;
+const uint8_t front_light_white = 15;
+const uint8_t front_light_yellow = 16;
+const uint8_t forward_brake = 8;
+const uint8_t reverse_brake = 9;
+const uint8_t rear_light = 10;
+const uint8_t limit_switch = 46;
 bool _connected = false;
-
+int limit_sw_State;
+int stflag=0;
 typedef enum {ZERO,ONE,TWO,THREE,FOUR,FIVE}number;
