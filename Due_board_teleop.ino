fde6ba173c1ebbff81b86b4fbe73826b96cd22c1
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-07-26 15:17:44 +0530

    Update Due_board_teleop.ino

--- Due_board_teleop.ino
+++ Due_board_teleop.ino
@@ -8,26 +8,26 @@
 #include <stdint.h>
 #include "teleop_config.h"
 
-
-
+//Arduino Due pwm library downloaded from forum.
+DuePWM pwm( frequency_right, frequency_left );
 
 // ROS serial server
-DuePWM pwm( frequency_right, frequency_left );
 ros::NodeHandle node;
-ros::Subscriber<std_msgs::Bool> sub_light1("front/light",&onfront);
-ros::Subscriber<std_msgs::Int16> sub_light2("front/light/multi",&onfont_multi);
-ros::Subscriber<geometry_msgs::Twist> sub("/linear/angular", &onTwist);
+ros::Subscriber<std_msgs::Bool> sub_light1("front/light",&onfront);//subscriber topic for front light
+ros::Subscriber<std_msgs::Int16> sub_light2("front/light/multi",&onfont_multi);//subscriber topic for front light fog or white
+ros::Subscriber<geometry_msgs::Twist> sub("/linear/angular", &onTwist);////subscriber topic for linear and angular values
 
 
+//setup function setting up all the functions,
 
 void setup()
 {
-  pwm.setFreq1( frequency_right );
-  pwm.setFreq2( frequency_left );
+  pwm.setFreq1( frequency_right );//First frquency used for left motors
+  pwm.setFreq2( frequency_left );// Second frequency used for right motor
   pinMode(L_PWM, OUTPUT);
   pinMode(R_PWM, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);// onboard led for testing
+  digitalWrite(LED_BUILTIN, LOW);
   pwm.pinFreq1( L_PWM );  // Pin 7 freq set to "pwm_freq2" on clock B
   pwm.pinFreq2( R_PWM );  // Pin 8 freq set to "pwm_freq2" on clock B
   
@@ -38,11 +38,21 @@ void setup()
   node.subscribe(sub_light2);
   
 }
+/*
+Settiup all GPIOs 
+GPIO3------> Right Direction Relay
+GPIO4------> Left Direction Relay
+GPIO14------> Front light relay
+GPIO15------> Front white light relay
+GPIO16------> Front yellow light Relay
+GPIO8------> forward_brake Relay
+GPIO6------> reverse_brake Relay rear_light
+GPI10------> rear_light Relay 
 
+*/
 void setupPins()
 {
    
-  pinMode(brake_light,OUTPUT);
   pinMode(right_relay, OUTPUT);
   pinMode(left_relay, OUTPUT);
   pinMode(front_light, OUTPUT);
@@ -50,16 +60,19 @@ void setupPins()
   pinMode(front_light_yellow, OUTPUT);
   pinMode(forward_brake, OUTPUT);
   pinMode(reverse_brake, OUTPUT);
-  pinMode(reverse_light, OUTPUT);
+  pinMode(rear_light, OUTPUT);
+  pinMode(limit_switch, INPUT_PULLUP);
   stop();
   
 }
 
 
-
+/*
+Stop Function is used to disable all relays initially
+PWM initialized as zero in boot up condition
+*/
 void stop()
 {
-    digitalWrite(brake_light, HIGH);
   digitalWrite(right_relay, HIGH);
   digitalWrite(left_relay, HIGH);
   digitalWrite(front_light, HIGH);
@@ -67,12 +80,22 @@ void stop()
   digitalWrite(front_light_yellow, HIGH);
   digitalWrite(forward_brake, HIGH); 
   digitalWrite(reverse_brake, HIGH);
-  digitalWrite(reverse_light, HIGH);
+  digitalWrite(rear_light, HIGH);
   pwm.pinDuty(L_PWM, 0);
   pwm.pinDuty(R_PWM, 0);
   
 }
+/*
+Twist call back function, this is used for the purpose of receiving or subscribing the values of linear and angular.
+linear and angular values publishing by the camera (Autonomous Mode) or (Teleop Mode)
 
+THis block contains code of converting linear in to motion.
+we are finding l value and r value to drive motors in corrosponding action
+if the l value is greater than 0, left direction relay is off
+       r value is greater than 0, right direction relay is off
+       l value is lesser than 0, left direction relay is on
+       r value is lesser than 0, right direction relay is on
+*/
 void onTwist(const geometry_msgs::Twist &msg)
 {
 
@@ -86,7 +109,6 @@ if(msg.linear.x>0||msg.linear.x<0)
   // Cap values at [-1 .. 1]
   
   // Calculate the intensity of left and right wheels. Simple version.
-  uint8_t right_flag=0,left_flag=0;
   float l = (msg.linear.x - msg.angular.z) / 2;
   float r = (msg.linear.x + msg.angular.z) / 2;
 
@@ -95,24 +117,24 @@ if(msg.linear.x>0||msg.linear.x<0)
   uint16_t rPwm = mapPwm(fabs(r), PWM_MIN, PWMRANGE);
 
   // Set direction pins and PWM
-       digitalWrite(left_relay, l<0);
-       digitalWrite(right_relay, r<0);
-  if(l<0 && r<0)
-  {
-    right_flag=1;
-    left_flag=1;
-  }
+   digitalWrite(left_relay, l<0);
+   digitalWrite(right_relay, r<0);
   digitalWrite(LED_BUILTIN, HIGH);
+  if(l<0 && r<0)
+   {
+     digitalWrite(rear_light, LOW);
+   }
+   else
+   {
+    digitalWrite(rear_light, HIGH);
+    }
   for(uint16_t i=25,j=25;i<lPwm,j<rPwm;i++,j++)
   {
    if(i<=lPwm)
-		pwm.pinDuty(L_PWM, i);
+    pwm.pinDuty(L_PWM, i);
    if(j<=rPwm)
-		pwm.pinDuty(R_PWM, j);
-   if(right_flag==1 && left_flag==1)
-   {
-     digitalWrite(reverse_light, LOW);
-   }
+    pwm.pinDuty(R_PWM, j);
+   
    delay(100);
   }
 }
@@ -121,7 +143,7 @@ else
   digitalWrite(LED_BUILTIN, LOW);
   pwm.stop(L_PWM);
   pwm.stop(R_PWM);
-  brake();
+  stflag=1;
 }
 }
 void onfront(const std_msgs::Bool &msg)
@@ -161,27 +183,29 @@ void onfont_multi(const std_msgs::Int16 &msg)
     digitalWrite(front_light_yellow, HIGH);
   }
 }
-void brake(void)
-{
-      digitalWrite(reverse_light, HIGH);
-      digitalWrite(brake_light, LOW);
-      digitalWrite(forward_brake, LOW); 
-      digitalWrite(reverse_brake, HIGH);
-      delay(1000);
-      digitalWrite(brake_light, HIGH);
-      digitalWrite(forward_brake, HIGH); 
-      digitalWrite(reverse_brake, LOW);
-      delay(1000);
-      digitalWrite(forward_brake, HIGH); 
-      digitalWrite(reverse_brake, HIGH);
-  
-}
+
 void loop()
 {
   if (!rosConnected())
     stop();
   node.spinOnce();
+   // read the state of the pushbutton value:
+  limit_sw_State = digitalRead(limit_switch);
 
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (limit_sw_State == HIGH && stflag) {
+    digitalWrite(forward_brake, LOW); 
+    digitalWrite(reverse_brake, HIGH);
+  }
+  if(limit_sw_State==LOW)
+  {
+    stflag=0;
+    digitalWrite(forward_brake, HIGH); 
+    digitalWrite(reverse_brake, LOW);
+    delay(2000);
+    digitalWrite(forward_brake, HIGH); 
+    digitalWrite(reverse_brake, HIGH);
+  }
 }
 
 bool rosConnected()
